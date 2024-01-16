6d0479051cc0b752fa5a18d1b5b217bed9b4771c
AndrewC <chena9654@parkwayschools.com>
2024-01-16 12:18:01 -0600

    Portfolio site

---
+++ src/data/user.js
@@ -0,0 +1,68 @@
+const info = {
+	main: {
+		title: "Portfolio by Andrew Chen",
+		name: "Andrew Chen",
+		email: "andgineering@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/AndchooChen",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer",
+		description:
+		"As a dedicated professional with a profound passion for creation, my journey began rooted in engineering, where I cultivated the ability to identify challenges and engineer effective solutions. This innate drive has seamlessly transitioned into the realm of software development. As a seasoned full-stack developer, I specialize in crafting scalable, secure, and reliable web and mobile applications. Proficient in technologies such as JavaScript and Node.js, I consistently uphold the highest industry standards in code quality, ensuring excellence in every project.",	},
+
+	projects: [
+		{
+			title: "FeedMe",
+			description:
+				"FeedMe is a project written in Javascript. The app implements a clean, sophisticated design to help its user track and manage their calories throughout the day. It uses React Native for frontend work, Next.js as the server, and the Edamam API as a database for food information.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/AndchooChen/FeedMe",
+		},
+
+		{
+			title: "Scrum",
+			description:
+				"Scrum is a Kahoot clone built using the Flutter framework. It has a sleek and colorful design. The project uses Firebase to store users and quizzes.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/dart/dart.png",
+			linkText: "View Project",
+			link: "https://github.com/AndchooChen/Scrum",
+		},
+
+		{
+			title: "Battle Bros",
+			description:
+				"In this game, the player can choose to play between two characters, Mario and Luigi, in a mortal combat battling system.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png",
+			linkText: "View Project",
+			link: "https://github.com/AndchooChen/BattleBros",
+		},
+
+		{
+			title: "Warfarin Dosage Calculator",
+			description:
+				"Warfarin is a drug.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Pomodoro Timer",
+			description:
+				"WIP.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default info;
---
+++ src/pages/contact.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import Socials from "../components/about/socials";
+
+import INFO from "../data/user";
+
+import "./styles/contact.css";
+
+const Contact = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Contact | ${INFO.main.title}`}</title>
+			</Helmet>
+
+			<div className="page-content">
+				<NavBar active="contact" />
+				<div className="content-wrapper">
+					<div className="contact-logo-container">
+						<div className="contact-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+
+					<div className="contact-container">
+						<div className="title contact-title">
+							Contact Me
+						</div>
+
+						<div className="subtitle contact-subtitle">
+							I appreciate you for looking at ways to contact me. If you have any questions or comments, you can email me at
+							&nbsp;{" "}
+							<a href={`mailto:${INFO.main.email}`}>
+								{INFO.main.email}
+							</a>
+							. I will do my best in trying to respond to all messages in a reasonable time frame. Other ways of contacting me includes the links I have to the socials below.
+							Thank you for showing your interesting in me.
+						</div>
+					</div>
+
+					<div className="socials-container">
+						<div className="contact-socials">
+							<Socials />
+						</div>
+					</div>
+
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Contact;
